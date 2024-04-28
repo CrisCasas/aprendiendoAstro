0164de7154ef5a1f05ac3cd21ed07425abbd7539
cristiancasas706 <cristiancasas706@gmail.com>
2024-04-27 22:55:08 -0500

    configurando Tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
+
+//importar tailwind en el archivo astro y que tipo de archivos puede usar
