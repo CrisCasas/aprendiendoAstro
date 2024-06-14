d226bf4348d795c00bedcc141a94b96f859828d8
criscasas <cristiancasas706@gmail.com>
2024-06-13 23:03:04 -0500

    seo rss y sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), vue(), svelte()] //integrar tailwind
+  site: 'https://localhost:4321',
+  integrations: [tailwind(), mdx(), react(), vue(), svelte(),sitemap()] //integrar tailwind
 });
